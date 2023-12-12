8fc7e4518f02a3d7459e95d6a5ae2cf8f424344b
Mohamed Aladdin <moham3daladdin101@gmail.com>
2023-12-12 10:50:41 +0000

    Fix memory leak in exec_cmnds fn

--- exec_cmnds.c
+++ exec_cmnds.c
@@ -14,32 +14,33 @@ int exec_cmnds(char ***new_env, char **arr)
 	char *cmnd;
 	pid_t pid = fork();
 
-	cmnd = malloc(sizeof(char) * (_strlen(arr[0]) + _strlen("/usr/bin/")));
-	if (!cmnd)
-	{
-		perror("malloc");
-		exit(EXIT_FAILURE);
-	}
-	_strcpy(cmnd, "/usr/bin/");
-	_strcat(cmnd, arr[0]);
 	if (pid == 0)
 	{
+		cmnd = malloc(sizeof(char) * (_strlen(arr[0]) + _strlen("/usr/bin/")));
+		if (!cmnd)
+		{
+			perror("malloc");
+			exit(EXIT_FAILURE);
+		}
+		_strcpy(cmnd, "/usr/bin/");
+		_strcat(cmnd, arr[0]);
 		if (access(cmnd, X_OK) == 0)
 		{
 			execve(cmnd, arr, *new_env);
 			perror("Command execution failed");
+			free(cmnd);
 			exit(EXIT_FAILURE);
 		}
 		else
 		{
 			perror("hsh");
 			value = 0;
+			free(cmnd);
 			exit(EXIT_FAILURE);
 		}
 	}
 	else if (pid > 0)
 	{
-		/*wait(NULL);*/
 		waitpid(pid, &value, 0);
 		return (WEXITSTATUS(value));
 	}
